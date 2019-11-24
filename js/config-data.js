f235b6108facaeaa5ac3dc4dcf8c2bc3b3ea8bfb
yifanzheng <zhengyifan1996@outlook.com>
2019-11-24 14:38:29 +0800

    简化项目，使其可以配置

---
+++ js/config-data.js
@@ -0,0 +1,40 @@
+var configData = {
+  // 祝福标题，内容最好 7 条。默认没有
+  title: [
+    "Happy",
+    "Birthday",
+    "God",
+    "Gao",
+    "G",
+    "H",
+    "H"
+  ],
+
+  // 生日祝福的内容，内容条数任意。每句话尽量不要超过 15 个字，不然展示效果可能不太好。
+  contents: [
+    "送给我",
+    "心爱的宝贝儿",
+    "今天是你的生日",
+    "这是我们在一起的",
+    "第一个生日哟",
+    "以后你的每个生日",
+    "我们都一起过",
+    "今年要吃好的喽哦",
+    "要把我家小猪猪喂得饱饱",
+    "然后抱起走",
+    "mua~"
+  ],
+
+  // 按钮描述，默认英文。可以修改成自己喜欢的。
+  btn_text: {
+    turn_on: "开始",
+    play: "音乐",
+    bannar_coming: "好像少点东西",
+    balloons_flying: "气球？",
+    cake_fadein: "蛋糕？",
+    light_candle: "蜡烛？",
+    wish_message: "生日快乐",
+    story: "A MESSAGE FOR YOU",
+  }
+
+}
---
+++ js/config-handler.js
@@ -0,0 +1,41 @@
+$(function() {
+  const title = configData.title;
+  if (title) {
+    let titleElement = null;
+    title.forEach((e, index) => {
+      titleElement = $('#b' + (index + 1));
+      titleElement.html(e);
+    });
+  }
+
+});
+
+/** 加载生日祝福的内容 */
+$(function() {
+  let spanElement = document.createElement('span');
+  const contents = configData.contents;
+  let pElement = null;
+  if (contents) {
+    contents.forEach(item => {
+      pElement = document.createElement('p');
+      pElement.innerHTML = item;
+      spanElement.appendChild(pElement);
+    });
+
+    $('#text-container').append(spanElement.innerHTML);
+  }
+});
+
+
+/** 设置按钮文字 */
+$(function() {
+  const btns = configData.btn_text;
+  if (btns) {
+    const btnKeys = Object.keys(btns);
+    let btnElement = null;
+    btnKeys.forEach(key => {
+      btnElement = $('#' + key);
+      btnElement.html(btns[key]);
+    });
+  }
+});
